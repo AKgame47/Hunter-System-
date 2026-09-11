f390963f08a37fa74b1ab8b8d836c6354b382350
AKgame47 <knanandhakrishnan4@gmail.com>
2026-09-11 12:11:33 +0530

    Fix Compose theme function syntax

--- android/app/src/main/java/com/akgaming/huntersystem/MainActivity.kt
+++ android/app/src/main/java/com/akgaming/huntersystem/MainActivity.kt
@@ -1,8 +1,20 @@
 package com.akgaming.huntersystem
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.akgaming.huntersystem.ui.HunterApp
 import com.akgaming.huntersystem.ui.theme.HunterTheme
-class MainActivity : ComponentActivity() { override fun onCreate(savedInstanceState: Bundle?) { super.onCreate(savedInstanceState);enableEdgeToEdge();setContent { HunterTheme { HunterApp() } } } }
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HunterTheme {
+                HunterApp()
+            }
+        }
+    }
+}
