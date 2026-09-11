f2d0df7666a76a6355fcef53f8de52aa8400c0fa
AKgame47 <knanandhakrishnan4@gmail.com>
2026-09-11 13:03:55 +0530

    Connect Android build to Supabase project

---
+++ android/app/src/main/java/com/akgaming/huntersystem/data/remote/SupabaseProvider.kt
@@ -0,0 +1,21 @@
+package com.akgaming.huntersystem.data.remote
+
+import com.akgaming.huntersystem.BuildConfig
+import io.github.jan.supabase.SupabaseClient
+import io.github.jan.supabase.auth.Auth
+import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.functions.Functions
+import io.github.jan.supabase.postgrest.Postgrest
+
+object SupabaseProvider {
+    val client: SupabaseClient by lazy {
+        createSupabaseClient(
+            supabaseUrl = BuildConfig.SUPABASE_URL,
+            supabaseKey = BuildConfig.SUPABASE_PUBLISHABLE_KEY,
+        ) {
+            install(Auth)
+            install(Postgrest)
+            install(Functions)
+        }
+    }
+}
